1f2d008113256193b51109653a32b5f440443a5b
SzokolAttila <szokol.ati2005@gmail.com>
2024-11-04 11:42:15 +0000

    imported necessary data

---
+++ backend/resources/views/plane/partials/card.blade.php
@@ -0,0 +1,15 @@
+<div class="col-12 col-md-6 col-lg-3 mb-2 d-flex align-items-stretch">
+    <div class="card">
+        <img src="" class="card-img-top" alt="">
+        <div class="card-body d-flex flex-column justify-content-evenly">
+            <h5 class="card-title"></h5>
+            <ul>
+                <li>Szélestörzsű Keskenytörzsű</li>
+                <li>Első felszállás: </li>
+                <li>Hajtóművek száma: </li>
+                <li>Maximum ülések száma: ülés</li>
+            </ul>
+            <a href="" class="btn btn-primary">További információk</a>
+        </div>
+    </div>
+</div>
