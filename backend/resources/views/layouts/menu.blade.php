a17aca4f4d85a31d1274c5054a061a95d894d713
SzokolAttila <szokol.ati2005@gmail.com>
2024-11-04 12:30:57 +0000

    fixed navbar

--- backend/resources/views/layouts/menu.blade.php
+++ backend/resources/views/layouts/menu.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg" style="background-color: cornflowerblue">
+<nav class="navbar navbar-expand-lg" style="background-color: cornflowerblue !important">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ route('home') }}">Repülők</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -10,10 +10,10 @@
                     <a class="nav-link" href="{{ route('home') }}">Összes</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('home', ['' => '']) }}">Airbus</a>
+                    <a class="nav-link" href="{{ route('home', ['manufacturer' => 'Airbus']) }}">Airbus</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('home', ['' => '']) }}">Boeing</a>
+                    <a class="nav-link" href="{{ route('home', ['manufacturer' => 'Boeing']) }}">Boeing</a>
                 </li>
             </ul>
         </div>
